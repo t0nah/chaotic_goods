f45dbe33681fd3578481b63d5760eb3c04c861d4
t0nah <tonah@academic.rrc.ca>
2024-12-01 22:13:28 -0600

    Added pagination to products page using kaminari, updated links and card styles for product displayon product page and product header

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    @categories = Category.all
+    @products = Kaminari.paginate_array(@products).page(params[:page]).per(10)
   end
 
   def show
