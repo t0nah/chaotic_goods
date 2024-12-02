6671bcce5e6b19eaa126679a9aaafb83a927c5da
t0nah <tonah@academic.rrc.ca>
2024-12-01 23:27:15 -0600

    Generated kaminari view setup and used active record to paginate without using an array

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     @categories = Category.all
-    @products = Kaminari.paginate_array(@products).page(params[:page]).per(10)
+    @products = @products.page(params[:page]).per(10)
   end
 
   def show
