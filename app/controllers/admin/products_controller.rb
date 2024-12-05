69b56f1062e4b6628fc5b8c9081c45387828152e
t0nah <tonah@academic.rrc.ca>
2024-12-04 21:42:27 -0600

    Completed setup of CRUD functions in aadmin dashboard

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,46 +1,61 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticate_admin!
+  before_action :set_product, only: [:show, :edit, :update, :destroy] # DRY approach
 
   def index
     @products = Product.all
   end
 
   def show
-    @products = Product.find(params[:id])
+    # @product is set by the before_action
   end
 
   def new
-    @products = Product.new
+    @product = Product.new
   end
 
   def create
-    @products = Product.new(product_params)
-    if @products.save
+    @product = Product.new(product_params)
+    if @product.save
       redirect_to admin_products_path, notice: 'Product was successfully created.'
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
-  def edit; end
+  def edit
+    # @product is set by the before_action
+  end
 
   def update
-    if @products.update(product_params)
-      redirect_to admin_products_path, notice: 'Product was successfully updated.'
+    if @product.update(product_params)
+      flash[:notice] = 'Product was successfully updated.'
+      redirect_to admin_products_path
     else
-      render :edit
+      flash.now[:alert] = 'Failed to update product. Please fix the errors below.'
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @products.destroy
-    redirect_to admin_products_path, notice: 'Product was successfully deleted.'
+    @product = Product.find(params[:id])
+    if @product.destroy
+      Rails.logger.info("Product #{@product.id} deleted successfully.")
+      redirect_to admin_products_path, notice: 'Product was successfully deleted.'
+    else
+      Rails.logger.error("Failed to delete product #{@product.id}.")
+      redirect_to admin_products_path, alert: 'Failed to delete the product.'
+    end
+  rescue StandardError => e
+    Rails.logger.error("Error during deletion: #{e.message}")
+    redirect_to admin_products_path, alert: 'An unexpected error occurred.'
   end
+
 
   private
 
   def set_product
-    @products = Product.find(params[:id])
+    @product = Product.find(params[:id]) # Singular instance variable for a single product
   end
 
   def product_params
